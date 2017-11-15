5ebe7ee10504381aa18c6b04df6d42ea01351756
odanoburu <bcclaro@gmail.com>
2017-11-15 19:16:26 -0200

    compiles, but seems to have mistakes

--- src/oab-parser.hs
+++ src/oab-parser.hs
@@ -1,16 +1,14 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
 import GHC.Generics
 
-import System.IO
 import System.Environment
 import Data.Char
 import Text.Parsec.Combinator
 import Text.ParserCombinators.Parsec.Char hiding (letter)
 import Text.ParserCombinators.Parsec.Prim
---import Control.Applicative hiding (many, optional)
---import Control.Monad
+import qualified Data.ByteString.Lazy as BL
 import Data.Aeson
+import Data.Aeson.Encode.Pretty
 import Data.List.Split
 
 --
@@ -47,8 +45,8 @@ symbol xs = lexeme (string xs)
 
 --
 -- data types
-data Exam = Exam { year :: Int,
-                   edition :: String,
+data Exam = Exam { year      :: Int,
+                   edition   :: String,
                    questions :: [Question]
                  } deriving (Generic,Show)
 
@@ -56,17 +54,17 @@ instance ToJSON Exam where
   toEncoding = genericToEncoding defaultOptions
 
 data Question = Question { number :: Int,
-                           valid :: Bool,
-                           enum :: String,
-                           items :: [Item]
+                           valid  :: Bool,
+                           enum   :: String,
+                           items  :: [Item]
                          } deriving (Generic,Show)
 
 instance ToJSON Question where
     toEncoding = genericToEncoding defaultOptions
 
-data Item = Item { letter :: Letter,
+data Item = Item { letter  :: Letter,
                    correct :: Bool,
-                   text :: String
+                   text    :: String
                  } deriving (Generic,Show)
 
 instance ToJSON Item where
@@ -116,7 +114,7 @@ itemLetter l = do
 
 itemCorrect :: Parser Bool
 itemCorrect = do
-  correct <-  choice [(symbol "CORRECT)"), (return [])]
+  correct <- option [] $ symbol "CORRECT)"
   return (notNull correct)
 
 itemA :: Parser Item
@@ -140,12 +138,14 @@ itemD = do
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let filename = (head args)
-  questions <- parseFromFile examQuestions filename
+  [filename] <- getArgs
+  result <- parseFromFile examQuestions filename
   let yearEdition = splitOn "-" filename
       (year, edition) = (yearEdition !! 0, yearEdition !! 1)
-  putStr
-    (encode $ Exam {year = (read year :: Int), edition = edition, questions = questions})
-
-example = "ENUM Questão 1\n\nJúlio e Lauro constituíram o mesmo advogado para,\njuntos, ajuizarem ação de interesse comum. No curso do processo,\nsobrevieram conflitos de interesse entre os constituintes, tendo\nJúlio deixado de concordar com Lauro com relação aos pedidos.\n\nNessa situação hipotética, deve o advogado\n\nOPTIONS\n\nA:CORRECT) optar, com prudência e discernimento, por um dos mandatos,\ne renunciar ao outro, resguardando o sigilo profissional.\n\nB) manter com os constituintes contrato de prestação de serviços\njurídicos no interesse da causa, resguardando o sigilo\nprofissional.\n\nC) assumir, com a cautela que lhe é peculiar, o patrocínio de\nambos, em ações individuais.\n\nD) designar, com prudência e cautela, por substabelecimento com\nreservas, um advogado de sua confiança.\n\n---\nENUM Questão 2\n\nMário, advogado regularmente inscrito na OAB, foi\ncondenado pela prática de crime hediondo e, após a sentença penal\ntransitada em julgado, respondeu a processo disciplinar, tendo\nsofrido, como consequência, penalidade de exclusão da Ordem. \n\nConsiderando a situação hipotética apresentada e o Estatuto da\nAdvocacia e da OAB, assinale a opção correta.\n\nOPTIONS\n\nA) Ainda que se reabilite criminalmente, Mário não poderá mais\nse inscrever na OAB, visto que não preenche o requisito de\nidoneidade moral.\n\nB) Serão considerados inexistentes os atos privativos de\nadvogado praticados por Mário após a exclusão, dado o\nimpedimento do exercício do mandato em razão da sanção\ndisciplinar aplicada.\n\nC) A penalidade de exclusão somente poderia ter sido aplicada\ncaso Mário tivesse recebido três suspensões.\n\nD:CORRECT) Supondo-se que o processo disciplinar tenha ficado paralisado\npor mais de três anos, aguardando o julgamento, a pretensão\nà punibilidade de Mário estaria prescrita e ele não poderia ser\nexcluído da Ordem.\n\n---"
+  case result of
+    Left err -> print err
+    Right qs ->
+      BL.putStr
+        (encodePretty $
+         Exam
+         {year = (read year :: Int), edition = edition, questions = qs})
